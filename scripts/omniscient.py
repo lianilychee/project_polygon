dc1c6efaf08204660cc1fb5cfa4bcc3287d09584
Liani Lye <lye.liani@gmail.com>
2015-11-16 19:40:24 -0500

    initialize node

--- scripts/omniscient.py
+++ scripts/omniscient.py
@@ -6,9 +6,34 @@ system.  Publish location of neighbor agents if agents are within a certain
 distance of each other.
 """
 
-import rospy
+import rospyfrom sensor_msgs.msg import LaserScan, Image
+from geometry_msgs.msg import Twist, PoseWithCovariance, Pose, Point, Vector3
+from sensor_msgs.msg import LaserScan, Image
 import tf
 import numpy as np
+
+class Omni:
+    def __init__(self):
+
+        ### ROS INITIALIZATION ###
+        rospy.init_node('omniscient')
+
+        # what am i subscribing to?
+        self.Subscriber('/robot1/odom')
+        self.Subscriber('/robot1/bump')
+        self.Subscriber('/robot2/odom')
+        self.Subscriber('/robot2/bump')
+        self.Subscriber('/robot3/odom')
+        self.Subscriber('/robot3/bump')
+        # what am I publiishing to?
+
+    def deploy(self):
+        """
+        deploys information to all agent nodes
+        """
+
+
+
 
 # subscribe to location of all bots
 
