7928340b6b3495cfeb58d396d255e0c37439861b
Liani Lye <lye.liani@gmail.com>
2015-11-23 14:26:23 -0500

    added sensing behaviour

--- sim/sim.py
+++ sim/sim.py
@@ -18,21 +18,22 @@ pos = np.array([    # bot pos
     (2.0, 3.0),
     (3.0, 2.0),
     (5.0, -3.0),
-    (0.0, -7.0)
+    (0.0, -20.0)
 ])
 vel = np.zeros(pos.shape)
 acc = np.zeros(pos.shape)
 
 tau = 0.1           # time step
 t = 0
-R = 3               # desired dist betw Bot and centroid
+R = 1               # desired dist betw Bot and centroid
 L = R*2*math.sin(math.pi/len(pos))    # desired dist betw 2 bots
 centroid = (3, 0)
 k_a = 0.08          # constant: betw other bots
 k_b = 0.2           # constant: acct for overshoot
 k_c = 0.08          # constant: betw centroid
 
-threshold = 0.1
+vel_threshold = 0.0001
+scan_threshold = 3  # how far a bot can "see"
 
 region = plt.Circle(centroid, R, color='r')
 plt.gcf().gca().add_artist(region)
@@ -41,8 +42,9 @@ plt.gcf().gca().add_artist(region)
 def euclid_dist(pt1, pt2):
     return math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
 
+state = True
 
-while True:
+while state:
 
     pos_temp = np.zeros(pos.shape)
     vel_temp = np.zeros(pos.shape)
@@ -59,9 +61,28 @@ while True:
         acc_xij = 0.0
         acc_yij = 0.0
 
+        # determine how many fellow bots are within range
+        scan_threshold
+
+        bot_temp = []
+
+        # for k in range(len(pos)):
+        #     if (i != k) and (euclid_dist(pos[i], pos[k]) < scan_threshold):
+        #         L = 
+        #         bot_count += bot_count
+            
+# L = R*2*math.sin(math.pi/len(pos))    # desired dist betw 2 bots
+
+        n = sum(1 for k in pos if euclid_dist(k, pos[i]) <= scan_threshold)
+
+        L = R*2*math.sin(math.pi/n)    # desired dist betw 2 bots
+
+        print L
+
+
         for j in range(len(pos)):
 
-            dij = euclid_dist(pos[i], pos[j])    # dist betw Bot and bot j
+            dij = euclid_dist(pos[i], pos[j])    # dist betw Bot and bot 
 
             # calc x- and y-accel betw Bot and bot j
             if (i != j) and (dij <= L):
@@ -70,15 +91,29 @@ while True:
 
         # update accels
         acc[i] = (acc_xci + acc_xij - k_b*vel[i][0], acc_yci + acc_yij - k_b*vel[i][1])
-        vel_temp[i] = vel[i] + acc[i] * tau
+        
+        vel_temp[i] = vel[i] + acc[i] * tau 
 
         pos_temp[i] = pos[i] + vel[i] * tau
 
-    # print acc
+
+        # if abs(vel[i][0] * tau) < threshold and abs(vel[i][1] * tau) < threshold:
+        # # if [abs(pos_temp[i] - pos[i]) > threshold]:
+        #     state = False
+
+
+    vel_temp[vel_temp > 0.5] = 0.5  # set neato vel upper bound
+    # vel_temp[vel_temp < 0.001] = 0  # set neato vel lower bound
     pos = pos_temp
     vel = vel_temp
 
-    plt.plot([p[0] for p in pos], [p[1] for p in pos], "o")
+
+
+
+    # print vel
+
+    # plt.plot([p[0] for p in pos], [p[1] for p in pos], "o")
+    plt.plot(pos[:,0], pos[:,1], "o")
 
     plt.pause(0.0001)
 
