45d9e403731d521f68e32ff024615ab53b73d2e4
Liani Lye <lye.liani@gmail.com>
2015-11-26 10:13:57 -0500

    fixed reference errors

--- scripts/helper_funcs.py
+++ scripts/helper_funcs.py
@@ -6,20 +6,14 @@ import math
 import numpy as np
 
 def euclid_dist(pt1, pt2):
-	"""
-	given two positions, calculate the euclid_dist
-	"""
+    """
+    given two positions, calculate the euclid_dist
+    """
     return math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
 
-def cal_resultant(x,y):
-	"""
-	given the x and y direction, calculate the resultant vector from component
-	velocity vectors.
-	"""
-	return math.sqrt(x**2+y**2)
 
 
-	
+    
 
 
 
